eb696593a428e6f0bb5013861f3bc5481949a395
Elmatador <kmouad29@gmail.com>
2020-07-26 19:22:54 -0600

    creditjs

--- index.js
+++ index.js
@@ -1,83 +1,57 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-// const conversion = require("phantom-html-to-pdf")();
-const axios = require("axios");
-const pdf = require("html-pdf");
-const emoji = require("node-emoji");
 
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "Enter your GitHub username:",
-      name: "username",
-    },
-    {
-      type: "list",
-      message: "What's your favorite color?",
-      name: "faveColor",
-      choices: ["red", "pink", "green", "blue"],
-    },
-  ]);
+const axios = require("axios");
+
+// const emoji = require("node-emoji");
+
+const questions = [
+  "Enter your full name:",
+  "Enter your email:",
+  "Enter your Github username:",
+  "Enter the name of the Github repo:",
+  "Project title:",
+  "Description:",
+  "Deployed page url:",
+  "Installation:",
+  "Usage:",
+  "Contributors:",
+  "(comma-delimited) Credits:",
+  "Enter ideas for Future Developments:",
+];
+
+let userPrompts = [];
+
+const Prompt = function (question) {
+  (this.type = "input"),
+    (this.message = question),
+    (this.name = this.message.split(" ").splice(-1).toString().toLowerCase());
+  this.validate = function (value) {
+    if (value.length) {
+      return true;
+    } else {
+      return "Input required";
+    }
+  };
+};
+
+for (i = 0; i < questions.length; i++) {
+  userPrompts[i] = new Prompt(questions[i]);
+  userPrompts[i].name = userPrompts[i].name.substring(
+    0,
+    userPrompts[i].name.length - 1
+  );
 }
 
-let readyToConvert = false;
-promptUser()
-  .then(function ({ username, faveColor }) {
-    const queryUrl = `https://api.github.com/users/${username}`;
-    axios.get(queryUrl).then(function (res) {
-      console.log(res);
-      info = {
-        color: faveColor,
-        profilePic: res.data.avatar_url,
-        name: res.data.login,
-        location: res.data.location,
-        profileUrl: res.data.html_url,
-        blog: res.data.blog,
-        bio: res.data.bio,
-        company: res.data.company,
-        repos: res.data.public_repos,
-        followers: res.data.followers,
-        following: res.data.following,
-      };
-      console.log(info);
+inquirer.prompt(userPrompts).then(function (response) {
+  let data = markdown(response);
 
-      const newQueryUrl = `https://api.github.com/users/${username}/repos`;
-      console.log(newQueryUrl);
-
-      axios.get(newQueryUrl).then(function (res) {
-        let starCount = 0;
-        for (let index = 0; index < res.data.length; index++) {
-          let count = res.data[index].stargazers_count;
-          starCount = starCount + count;
-        }
-        console.log("Final star count for all repositories: " + starCount);
-        info.starCount = starCount;
-        const html = generateHTML(info);
-
-        console.log(`${username}.html is ready to convert to PDF`);
-        readyToConvert = true;
-
-        // for testing the HTML file that gets written to disk
-        fs.writeFileSync(`${username}.html`, html);
-
-        // https://www.npmjs.com/package/html-pdf
-        var options = { format: "landscape" };
-        pdf
-          .create(html, options)
-          .toFile(`${username}.pdf`, function (err, res) {
-            if (err) return console.log(err);
-            console.log(res);
-          });
-      });
-    });
-    // })
-    //.then(function () {
-
-    // })
-
-    //.then(function () {
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+  (async () => {
+    try {
+      await writeToFile("README_.md", data);
+      await console.log(`${checkmark}  README.md successfully created!`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })();
+});
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,51 @@
-function generateMarkdown(data) {
-  return `
-# ${data.title}
+const generateCredits = require("./credits.js");
 
-`;
+function generateMarkdown(data) {
+  let credits = data.credits;
+  let links = generateCredits(credits);
+  return `
+  # Title: ${data.title}
+  ![last commit](https://img.shields.io/github/last-commit/${data.username}/${
+    data.repo
+  }?style=flat-square)![license badge](https://img.shields.io/github/license/${
+    data.username
+  }/${data.repo}?style=flat-square)
+  ### URL: ${data.url}
+  ## Description 
+  
+  ${data.description}
+  ## Table of Contents 
+   
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Future Developments](#future-developments)
+  * [Credits](#credits)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation 
+  \`\`\
+  ${data.installation}
+  \`\`\
+  
+  ## Usage 
+  
+  ${data.usage}
+  ## Future Developments
+  ${data.future - developments}
+  ## Credits
+  ${links}
+  ## Contributors: ${data.contributions}
+  ## Questions?
+  Contact me at  [${data.email}](mailto:${data.email}]
+  
+  or [![Follow on Github](https://img.shields.io/github/followers/${
+    data.username
+  }?label=Follow&style=social)](http://www.github.com/${data.username})
+  Copyright © 2020 [${data.name}](http://www.github.com/${data.username})
+  ---
+  
+  
 }
 
 module.exports = generateMarkdown;
