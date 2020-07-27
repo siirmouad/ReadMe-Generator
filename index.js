cbe95548a803439c8018cc00ffd38c4e692cbdfb
siirmouad <61400561+siirmouad@users.noreply.github.com>
2020-07-26 23:10:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const axios = require("axios");
 const writeToFile = require("write-to-file");
 const emoji = require("node-emoji");
 const markdown = require("./utils/generateMarkdown.js");
-// const github = require("./utils/github.js");
 
-// emojis
-const checkmark = emoji.get("heavy_check_mark");
 
-// command line prompts
+
+
+
 const questions = [
   "Enter your full name:",
   "Enter your email:",
@@ -26,7 +25,6 @@ const questions = [
 
 let prompts = [];
 
-// contructor function to create prompt object for each question
 const Prompt = function (question) {
   (this.type = "input"),
     (this.message = question),
@@ -40,13 +38,12 @@ const Prompt = function (question) {
   };
 };
 
-// generates an array of prompt objects to pass into inquirer
 for (i = 0; i < questions.length; i++) {
   prompts[i] = new Prompt(questions[i]);
   prompts[i].name = prompts[i].name.substring(0, prompts[i].name.length - 1);
 }
 
-// command line prompts
+
 inquirer.prompt(prompts).then(function (response) {
   let data = markdown(response);
 
